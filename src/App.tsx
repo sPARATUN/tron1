395403b2f4a21b800bc86c19fcc897e3ac2a3637
sPARATUN <sparatunf@gmail.com>
2025-05-25 22:39:34 +0300

    Boosssst4

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-// src/App.tsx
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './components/layout/Layout'; // относительный путь, default-экспорт
-
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<div />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+--- src/App.tsx
+- import React from 'react';
+  import { BrowserRouter, Routes, Route } from 'react-router-dom';
+  import Layout from './components/layout/Layout';
+  
+  export default function App() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<div />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    );
+  }
